d85d52ef946e6b6ac184a90041a3a7d65cc6585b
nuno-21902803 <nunomaluko5@gmail.com>
2022-05-13 10:44:11 +0100

    n ligar

---
+++ app/src/main/java/com/example/acalculator/CalculatorApplication.kt
@@ -0,0 +1,19 @@
+package com.example.acalculator
+
+import android.app.Application
+
+class CalculatorApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        CalculatorRepository.init(
+            this,
+            CalculatorRoom(CalculatorDatabase.getInstance(this).operationDao()),
+            CalculatorRetrofit(
+                RetrofitBuilder.getInstance(
+                    "https://cm-calculadora.herokuapp.com/api/"
+                )
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/acalculator/CalculatorRepository.kt
@@ -0,0 +1,78 @@
+package com.example.acalculator
+
+import android.annotation.SuppressLint
+import android.content.Context
+import java.lang.IllegalStateException
+
+class CalculatorRepository(
+    private val context: Context,
+    private val local: CalculatorModel,
+    private val remote: CalculatorModel
+) {
+
+    fun getExpression() = local.expression
+
+    fun insertSymbol(symbol: String): String{
+        return remote.insertSymbol(symbol)
+    }
+
+    fun clear(): String {
+        return remote.clearDisplay()
+    }
+
+    fun deleteLastSymbol(): String {
+        return remote.insertSymbol("C")
+    }
+
+    fun performOperation(onFinished: () -> Unit) {
+        remote.performOperation {
+            local.expression = remote.expression
+            onFinished()
+        }
+    }
+
+    fun getLastOperation(onFinished: (String) -> Unit) {
+        if(ConnectivityUtil.isOnline(context)) {
+            remote.getLastOperation(onFinished)
+        } else {
+            local.getLastOperation(onFinished)
+        }
+    }
+
+    fun deleteOperation(uuid: String, onSuccess: () -> Unit) {
+        remote.deleteOperation(uuid, onSuccess)
+    }
+
+    fun getHistory(onFinished: (List<OperationUI>) -> Unit) {
+        if(ConnectivityUtil.isOnline(context)) {
+            remote.getHistory { history ->
+                local.deleteAllOperations {
+                    local.insertOperations(history) {
+                        onFinished(history)
+                    }
+                }
+            }
+        } else {
+            local.getHistory(onFinished)
+        }
+    }
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        private var instance: CalculatorRepository? = null
+
+        fun init(context: Context, local: CalculatorModel, remote: CalculatorModel) {
+            synchronized(this){
+                if(instance == null){
+                    instance = CalculatorRepository(context, local, remote)
+                }
+            }
+        }
+
+        fun getInstance(): CalculatorRepository{
+            return instance ?:
+            throw IllegalStateException("Repo not initialized")
+        }
+
+    }
+}
